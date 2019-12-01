67b054cab16a551b429bd09a9a260abba964af8f
nikhithabhupal2000 <nikhithabhupal2000@gmail.com>
2019-12-01 07:11:08 +0530

    Client and server can run independently

--- chat_application/MyClient.java
+++ chat_application/MyClient.java
@@ -1,20 +1,44 @@
 import java.net.*;  
-import java.io.*;  
+import java.io.*;
+import java.sql.*;
+import java.util.*;  
 class MyClient{  
-public static void main(String args[])throws Exception{  
-	Socket s=new Socket("localhost",3333);
-	DataInputStream din=new DataInputStream(s.getInputStream());  
-	DataOutputStream dout=new DataOutputStream(s.getOutputStream());  
-	BufferedReader br=new BufferedReader(new InputStreamReader(System.in));    
-	String str="",str2="";  
-	while(!str.equals("stop")){  
-		str=br.readLine();  
-		dout.writeUTF(str);  
-		dout.flush();  
-		str2=din.readUTF();  
-		System.out.println("Server says: "+str2);  
-	}    
-	dout.close();  
-	s.close();  
+public static void main(String args[])throws Exception{
+	try{  
+		Socket s=new Socket("localhost",3333);
+		DataInputStream din=new DataInputStream(s.getInputStream());  
+		DataOutputStream dout=new DataOutputStream(s.getOutputStream());  
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));    
+		String str="",str2="";  
+		while(!str.equals("stop")){  
+			str=br.readLine();  
+			dout.writeUTF(str);  
+			dout.flush();  
+			str2=din.readUTF();  
+			System.out.println("Server says: "+str2);  
+		}    
+		dout.close();  
+		s.close();
+	}catch(ConnectException  | EOFException e){
+		Class.forName("com.mysql.jdbc.Driver"); // registering and loading the driver
+                String url = "jdbc:mysql:/localhost:3306/chat_application"; // local host is the name of the server containing the db and trial is the name of the database under usage;
+                String user = "root";
+                String password = "root";
+                Connection con = DriverManager.getConnection(url,user,password); // getting connected to database
+                Statement s = con.createStatement();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String msg = "";
+		int i = 1;
+		while(!msg.equals("stop")){
+			System.out.println("Enter the message");
+			msg = br.readLine();
+			String sql = "insert into messages(msg) values('"+msg+"')";
+               		 s.executeUpdate(sql);
+			 i ++;
+
+
+	}
 	}
 }
+}
+
--- chat_application/MyServer.java
+++ chat_application/MyServer.java
@@ -1,7 +1,24 @@
 import java.net.*;  
-import java.io.*;  
+import java.io.*;
+import java.sql.*;
+import java.util.*;  
 class MyServer{  
-public static void main(String args[])throws Exception{  
+public static void main(String args[])throws Exception{
+	Class.forName("com.mysql.jdbc.Driver");
+	String user = "root";
+	String pass = "root";
+	String url =  "jdbc:mysql:/localhost:3306/chat_application"; 
+	Connection con = DriverManager.getConnection(url,user,pass);
+	Statement st = con.createStatement();
+	ResultSet rs;
+	String sql = "Select * from messages";
+	rs = st.executeQuery(sql);
+	while(rs.next()){
+		System.out.println(rs.getString(1));
+	}
+	rs.close();
+	sql = "delete from messages";
+	st.executeUpdate(sql);
 	ServerSocket ss=new ServerSocket(3333);
 	Socket s=ss.accept();  
 	DataInputStream din=new DataInputStream(s.getInputStream());  
